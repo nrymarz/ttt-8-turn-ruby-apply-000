95cd2dfdbff2d5ff74454f370c4936e55c85b675
Nathan Rymarz <nrymarz@gmail.com>
2020-09-25 02:37:35 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,12 +1,20 @@
 def display_board(board)
-
+puts " #{board[0]} | #{board[1]} | #{board[2]} "
+puts "-----------"
+puts " #{board[3]} | #{board[4]} | #{board[5]} "
+puts "-----------"
+puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
 def valid_move?(board,index)
-  
+  if board[index] == " "
+    true 
+  end 
 end
 
-def move(board,index)
-  
+def move(board,index,char="X")
+  board[index] = char
 end
+
+def turn
 
